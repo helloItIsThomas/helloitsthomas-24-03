183b292e829bb09a513fda3520eaf3618b4eb018
Thomas McElmeel <thomasmcelmeel@Thomass-MacBook-Pro.local>
2024-07-19 22:34:47 -0400

    ~

--- src/js/handleInteraction.js
+++ src/js/handleInteraction.js
@@ -10,26 +10,35 @@ function homeButton() {
 homeButton();
 
 function topFunction() {
-  projectContent.scrollTop = 0;
-  projectContent.scrollTop = 0;
+  gsap.to(projectContent, {
+    scrollTop: 0,
+    duration: 0.5, // Adjust duration as needed
+    ease: "power2.inOut",
+    onComplete: () => {
+      resetClip();
+    },
+  });
 }
 
 function toHome() {
-  topFunction();
-  globalState.isHome = true;
-  console.log("toHome");
-  projectContent.style.overflowY = "hidden";
-  resetClip();
-  const thumb = document.getElementById("projectFrontC");
-  thumb.style.display = "block";
+  if (!globalState.isHome) {
+    topFunction();
+    globalState.isHome = true;
+    console.log("toHome");
+    projectContent.style.overflowY = "hidden";
+    const thumb = document.getElementById("projectFrontC");
+    thumb.style.display = "block";
+  }
 }
 function toProject() {
-  globalState.isHome = false;
-  console.log("toProject");
-  const thumb = document.getElementById("projectFrontC");
-  resizeClip();
-  shrinkKnobBar();
-  projectContent.style.overflowY = "scroll";
+  if (globalState.isHome) {
+    globalState.isHome = false;
+    console.log("toProject");
+    const thumb = document.getElementById("projectFrontC");
+    resizeClip();
+    shrinkKnobBar();
+    projectContent.style.overflowY = "scroll";
+  }
 }
 
 function toggleScrollBehavior() {
@@ -41,21 +50,91 @@ function toggleScrollBehavior() {
 }
 
 function resetClip() {
-  const clipPathValue = `rect(0 0 100% 100% round 0vw)`;
-  const elementToClip = document.querySelector("#thumbnail img");
-  elementToClip.style.clipPath = clipPathValue;
-}
-
-function resizeClip() {
+  const parentHeight = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner")
+  ).height;
+  const parentWidth = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner")
+  ).width;
   const row1Height = window.getComputedStyle(
     document.querySelector(".projectBackCBanner .row1")
   ).height;
   const row2Height = window.getComputedStyle(
     document.querySelector(".projectBackCBanner .row2")
   ).height;
-  const clipPathValue = `rect(${row1Height} 0 calc(${row1Height} + ${row2Height}) 100% round 5vw)`;
-  const elementToClip = document.querySelector("#thumbnail img");
-  elementToClip.style.clipPath = clipPathValue;
+
+  const parentWidthDouble = parseFloat(parentWidth);
+  const parentHeightDouble = parseFloat(parentHeight);
+
+  const row1HeightDouble = parseFloat(row1Height);
+  const row2HeightDouble = parseFloat(row2Height);
+
+  const myRect = {
+    top: row1HeightDouble,
+    right: parentWidthDouble,
+    bottom: row1HeightDouble + row2HeightDouble,
+    left: 0,
+    radius: 30,
+  };
+
+  gsap.to(myRect, {
+    top: 0,
+    right: parentWidthDouble,
+    bottom: parentHeightDouble,
+    left: 0,
+    radius: 0,
+    duration: 0.0666,
+    onUpdate: () => {
+      const clipPathValue = `rect(${myRect.top}px ${myRect.right}px ${myRect.bottom}px ${myRect.left}px round ${myRect.radius}px)`;
+      const elementToClip = document.querySelector("#thumbnail img");
+
+      elementToClip.style.clipPath = clipPathValue;
+    },
+  });
+}
+
+function resizeClip() {
+  const parentHeight = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner")
+  ).height;
+  const parentWidth = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner")
+  ).width;
+  const row1Height = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner .row1")
+  ).height;
+  const row2Height = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner .row2")
+  ).height;
+
+  const parentWidthDouble = parseFloat(parentWidth);
+  const parentHeightDouble = parseFloat(parentHeight);
+
+  const myRect = {
+    top: 0,
+    right: parentWidthDouble,
+    bottom: parentHeightDouble,
+    left: 0,
+    radius: 0,
+  };
+
+  const row1HeightDouble = parseFloat(row1Height);
+  const row2HeightDouble = parseFloat(row2Height);
+
+  gsap.to(myRect, {
+    top: row1HeightDouble,
+    right: parentWidthDouble,
+    bottom: row1HeightDouble + row2HeightDouble,
+    left: 0,
+    radius: 30,
+    duration: 0.0666,
+    onUpdate: () => {
+      const clipPathValue = `rect(${myRect.top}px ${myRect.right}px ${myRect.bottom}px ${myRect.left}px round ${myRect.radius}px)`;
+      const elementToClip = document.querySelector("#thumbnail img");
+
+      elementToClip.style.clipPath = clipPathValue;
+    },
+  });
 }
 
 function shrinkKnobBar() {
