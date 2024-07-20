692a38d08b715b83a0b893527c815ca732a82e87
Thomas McElmeel <thomasmcelmeel@Thomass-MacBook-Pro.local>
2024-07-19 21:25:45 -0400

    ~

--- src/js/handleInteraction.js
+++ src/js/handleInteraction.js
@@ -1,37 +1,66 @@
-function toggleScrollBehavior(pc) {
-  const projectContentRef = window.getComputedStyle(pc);
-  // projectContent.style.overflowY =
-  // projectContent.style.overflowY === "scroll" ? "hidden" : "scroll";
+const globalState = {
+  isHome: true,
+};
+
+function homeButton() {
+  document.getElementById("logo").addEventListener("click", () => {
+    toHome();
+  });
+}
+homeButton();
+
+function topFunction() {
+  projectContent.scrollTop = 0;
+  projectContent.scrollTop = 0;
 }
 
-function resizeTextToFit() {
-  // const paragraphs = document.querySelectorAll("#projectBackC p");
-  // const p = paragraphs[0];
-  // let fontSize = 40;
-  // p.style.fontSize = fontSize + "px";
-  // while (p.scrollWidth > p.clientWidth) {
-  // fontSize--;
-  // p.style.fontSize = fontSize + "px";
-  // }
+function toHome() {
+  topFunction();
+  globalState.isHome = true;
+  console.log("toHome");
+  projectContent.style.overflowY = "hidden";
+  resetClip();
+  const thumb = document.getElementById("projectFrontC");
+  thumb.style.display = "block";
+}
+function toProject() {
+  globalState.isHome = false;
+  console.log("toProject");
+  const thumb = document.getElementById("projectFrontC");
+  resizeClip();
+  shrinkKnobBar();
+  projectContent.style.overflowY = "scroll";
+}
+
+function toggleScrollBehavior() {
+  if (projectContent.style.overflowY === "scroll") {
+    // toHome();
+  } else {
+    // toProject();
+  }
+}
+
+function resetClip() {
+  const clipPathValue = `rect(0 0 100% 100% round 0vw)`;
+  const elementToClip = document.querySelector("#thumbnail img");
+  elementToClip.style.clipPath = clipPathValue;
 }
 
 function resizeClip() {
-  // const paragraphs = document.getElementById("#projectBackCBanner");
-  // const clipZone = window.getComputedStyle(
-  // document.querySelector("#projectBackCBanner p")
-  // ).height;
-  // const clipZoneBottom = window.getComputedStyle(
-  // document.querySelector("#projectBackCBanner")
-  // ).height;
-  // const clipPathValue = `rect(calc(calc(${clipZone}) + 0%) 0 ${clipZoneBottom} 100% round 5vw)`;
-  // const elementToClip = document.querySelector("#thumbnail img");
-  // elementToClip.style.clipPath = clipPathValue;
+  const row1Height = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner .row1")
+  ).height;
+  const row2Height = window.getComputedStyle(
+    document.querySelector(".projectBackCBanner .row2")
+  ).height;
+  const clipPathValue = `rect(${row1Height} 0 calc(${row1Height} + ${row2Height}) 100% round 5vw)`;
+  const elementToClip = document.querySelector("#thumbnail img");
+  elementToClip.style.clipPath = clipPathValue;
 }
 
 function shrinkKnobBar() {
-  console.log("shrinkKnobBar");
   const knobBar = document.querySelector("#knobBar");
-  knobBar.style.width = "10px";
+  // knobBar.style.width = "10px";
 }
 
 function barToBack() {
